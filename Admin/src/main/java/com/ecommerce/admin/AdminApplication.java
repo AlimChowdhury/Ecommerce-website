b8e9dc100ab4777b2a8a8a3c5556d62f82da18c6
AlimChowdhury <md.alim.chowdhury8@gmail.com>
2023-10-24 18:38:29 +0600

    add product

--- Admin/src/main/java/com/ecommerce/admin/AdminApplication.java
+++ Admin/src/main/java/com/ecommerce/admin/AdminApplication.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication(scanBasePackages = {"com.ecommerce.library.*", "com.ecommerce.admin.*"})
 @EnableJpaRepositories(value = "com.ecommerce.library.repository")
 @EntityScan(value = "com.ecommerce.library.model")
-public class AdminApplication {
+public class  AdminApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
